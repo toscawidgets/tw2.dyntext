4c444f94fcd6d2d33adc5b8a5b7ceb3f5445b880
Greg Jurman <gdj2214@rit.edu>
2012-02-20 14:08:35 -0500

    Fixed widget to conform to tests

--- tw2/dyntext/templates/dyntext.mak
+++ tw2/dyntext/templates/dyntext.mak
@@ -1,5 +1,2 @@
 <%namespace name="tw" module="tw2.core.mako_util"/>
 <span id="${w.attrs['id']}">${w.initial_text}</span>
-<script type="text/javascript">
-    setupPollingDynText("${w.selector}", ${w.j('data_url')}, ${w.j('interval')});
-</script>
--- tw2/dyntext/widgets.py
+++ tw2/dyntext/widgets.py
@@ -4,6 +4,8 @@ import tw2.core as twc
 from tw2.jquery import jquery_js
 import json
 
+setup_call = twc.js_function('setupPollingDynText')
+
 class DynamicTextWidget(twc.Widget):
     template = "mako:tw2.dyntext.templates.dyntext"
 
@@ -29,4 +31,5 @@ class DynamicTextWidget(twc.Widget):
 
         super(DynamicTextWidget, self).prepare()
         # put code here to run just before the widget is displayed
-        self.selector = self.attrs['id'].replace(':', '\\\\:')
+        self.selector = self.attrs['id'].replace(':', '\\:')
+        self.add_call(setup_call(self.selector, self.data_url, self.interval))
