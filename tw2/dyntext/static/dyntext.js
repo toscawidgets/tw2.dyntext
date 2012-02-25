8c5924de96d87e3b0c2052c3030970d02cf1bb0e
Greg Jurman <gdj2214@rit.edu>
2012-02-25 00:12:31 -0500

    Moved the text setter call into it's own function so it's easily callable.

--- tw2/dyntext/static/dyntext.js
+++ tw2/dyntext/static/dyntext.js
@@ -1,9 +1,10 @@
-function setupPollingDynText(text_widget, data_url, interval)
-{
+function setDynamicText(text_widget, new_value) {
+    $("#"+text_widget).text(new_value);
+}
+
+function setupPollingDynText(text_widget, data_url, interval) {
     $.getJSON(data_url, function(data) {
-        var new_text = data.text;
-        
-        $("#"+text_widget).text(new_text);
+        setDynamicText(text_widget, data.text);
     });
 
     setTimeout( function () {
